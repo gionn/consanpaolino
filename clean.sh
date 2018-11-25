54449f1de1599cf6bf39668c7b2b3a7b31c520c6
Giovanni Toraldo <me@gionn.net>
2018-11-25 10:09:44 +0100

    Clean all files

--- clean.sh
+++ clean.sh
@@ -1,2 +1,2 @@
-find . -iname "*.jpg" -exec jpegoptim --strip-all -m85 -o -p {} \;
+find . -iname "*.jpg" -exec jpegoptim --strip-all --all-progressive {} \;
 find . -iname "*.png" -exec optipng -clobber -preserve -strip all -o2 {} \;
